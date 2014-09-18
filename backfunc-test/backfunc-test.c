b4d1f9a66697f5e9373b39209a7820ac1e3459d9
leyap <leyapin@gmail.com>
2014-09-18 11:48:02 +0800

    v0.1

--- backfunc-test/backfunc-test.c
+++ backfunc-test/backfunc-test.c
@@ -5,15 +5,13 @@ int buf[20] = {0,1,2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 //
 void backfunc (int *buf, int a, int b) {
 	int mid = (a+b)/2;
-	if (b -a < 2) {
-		if (a == b)
+	printf ("<%d, %d, %d>\n", a, mid, b);
+	if (b -a < 1) {
 			printf ("%d\n", a);
-		else
-			printf ("%d,%d\n", a, b);
 		return;
+	} else if (b-a > 1)
+	printf ("%d\n", mid);
 
-	} else
-		printf ("%d\n", mid);
 	backfunc (buf, a, mid-1);
 	backfunc (buf, mid+1, b);
 }
